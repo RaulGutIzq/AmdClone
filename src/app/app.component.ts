4ae97aa66ed9d4dded2dda5578d23360b27e44a7
Raúl Gutiérrez <167849538+RaulGutIzq@users.noreply.github.com>
2025-02-06 09:27:08 +0100

    estilo ngfor arreglado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'amd';
+export class AppComponent implements OnInit {
+  products: Product[] = [];  // Declare the property
+
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
