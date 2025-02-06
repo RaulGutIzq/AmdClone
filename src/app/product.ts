721fda381e56940a894317bf3285748b24bc0ac2
Raúl Gutiérrez <167849538+RaulGutIzq@users.noreply.github.com>
2025-02-06 09:11:41 +0100

    ngFor

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,8 +1,11 @@
 export interface Product {
   id: number;
+  title: string;
   name: string;
   description: string;
   price: number;
+  image: string;
   imageUrl: string;
+  link: string;
   category: string;
 }
